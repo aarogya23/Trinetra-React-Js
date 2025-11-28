9583ca2f3de561522b4c44941c75db7a9bd9d3db
unknown <aarogyathapa375@gmail.com>
2025-11-28 13:58:17 +0545

    Update on the code level

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+
 import logo from "./assets/image/trinetra.png";
 function LoginPage() {
   const [email, setEmail] = useState('');
